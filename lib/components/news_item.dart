4ff460578275c87779453b684e923805c26fd65a
StefanoGR <at.stefano.rago@gmail.com>
2020-04-29 22:01:02 +0200

    fix ui

--- lib/components/news_item.dart
+++ lib/components/news_item.dart
@@ -9,7 +9,6 @@ import 'package:provider/provider.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 class NewsItem extends StatefulWidget {
-
   final Article article;
 
   NewsItem(this.article, {Key key}) : super(key: key);
@@ -27,36 +26,41 @@ class _NewsItemState extends State<NewsItem> {
     favoriteBox = Hive.box(NewsBox);
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return InkWell(onTap: onFavoritePress, child: Container(
-        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            LimitedBox(
-              maxHeight: 300,
-              child: kIsWeb ? NetworkImage(widget.article.urlToImage):CachedNetworkImage(
-                imageUrl: widget.article.urlToImage,
-                placeholder: (context, url) =>
-                    center(CircularProgressIndicator()),
-                errorWidget: (context, url, error) => center(Icon(Icons.image)),
-              ),
-            ),
-            Text(widget.article.author != null ? widget.article.author : ""),
-            const SizedBox(
-              height: 8,
-            ),
-            Text(widget.article.title),
-            const SizedBox(
-              height: 8,
-            ),
-            Text(widget.article.publishedAt),
-            getIcon(widget.article.id)
-
-          ],
-        )));
+    return InkWell(
+        onTap: onFavoritePress,
+        child: Container(
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                LimitedBox(
+                    maxHeight: 300,
+                    child: Center(
+                      child: kIsWeb
+                          ? Image.network(widget.article.urlToImage)
+                          : CachedNetworkImage(
+                              imageUrl: widget.article.urlToImage,
+                              placeholder: (context, url) =>
+                                  center(CircularProgressIndicator()),
+                              errorWidget: (context, url, error) =>
+                                  center(Icon(Icons.image)),
+                            ),
+                    )),
+                Text(
+                    widget.article.author != null ? widget.article.author : ""),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(widget.article.title),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(widget.article.publishedAt),
+                getIcon(widget.article.id)
+              ],
+            )));
   }
 
   Widget getIcon(String id) {
@@ -66,7 +70,6 @@ class _NewsItemState extends State<NewsItem> {
     return Icon(Icons.favorite_border);
   }
 
-
   void onFavoritePress() {
     if (favoriteBox.containsKey(widget.article.id)) {
       favoriteBox.delete(widget.article.id);
@@ -75,7 +78,6 @@ class _NewsItemState extends State<NewsItem> {
     favoriteBox.put(widget.article.id, widget.article);
   }
 
-  center(Widget widget) =>
-      Container(
-          height: 200, color: Colors.grey[200], child: Center(child: widget));
+  center(Widget widget) => Container(
+      height: 200, color: Colors.grey[200], child: Center(child: widget));
 }
